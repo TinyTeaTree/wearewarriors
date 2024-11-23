af692a7dcac66881180114f3f34fdaa042174f5a
Tomer Asraf <tomerasraf2@gmail.com>
2024-11-24 00:44:44 +0200

    Update PlantVisual.cs

--- Assets/Scripts/Features/Garden/PlantVisual.cs
+++ Assets/Scripts/Features/Garden/PlantVisual.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections;
 using Core;
-using Unity.VisualScripting.YamlDotNet.Core;
 using UnityEngine;
 
 namespace Game
