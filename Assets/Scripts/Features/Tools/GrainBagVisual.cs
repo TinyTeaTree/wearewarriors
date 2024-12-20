3e6f22a99d8e34ede649e20dc040ab3b80d97f1a
Tomer Asraf <tomerasraf2@gmail.com>
2024-12-20 03:10:05 +0200

    Grain Bag poff

---
+++ Assets/Scripts/Features/Tools/GrainBagVisual.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+namespace Game
+{
+    public class GrainBagVisual : ToolVisual
+    {
+        public MeshRenderer BagMaterial;
+        public ParticleSystem PoffEffect;
+        public int SeedsVolume;
+        
+    }
+}
